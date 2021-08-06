c74638eef922265cdfa91f00518ebf5bbc667284
Rick <dejonge00@live.nl>
2021-08-06 17:27:41 +0200

    Refresh file list with .r, use num0 in song selection

--- core/KeyListener.py
+++ core/KeyListener.py
@@ -1,10 +1,12 @@
 from pynput.keyboard import Listener, Key
 
+from core.SoundEngine import SoundEngine
+
 modifiers_keys = [Key.alt_gr, Key.alt_l, Key.shift_l, Key.shift_r, Key.ctrl_l, Key.ctrl_r]
 
 
 class KeyListener(Listener):
-    def __init__(self, sound_engine):
+    def __init__(self, sound_engine: SoundEngine):
         self.sound = sound_engine
         self.modifiers = {k: False for k in modifiers_keys}
         self.long_input = [False, 0]
@@ -18,15 +20,19 @@ class KeyListener(Listener):
             return
         # print(key)
 
-        # Key functions with irregular keys
+        # Key functions
+        if key.char == 'r' and self.long_input[0]:
+            self.sound.load_files()
+            return
+
+            # Key functions with irregular keys
         if not hasattr(key, 'vk'):
             return
-        # Numpad 0
-        if key.vk == 96:
-            self.sound.stop_playing()
-            return
-        # Numpad 1-9
-        if 97 <= key.vk <= 105:
+        # Numpad 0-9
+        if 96 <= key.vk <= 105:
+            if key.vk == 96 and not self.long_input[0]:
+                self.sound.stop_playing()
+                return
             sound_nr = key.vk - 96
             if self.long_input[0]:
                 self.long_input[1] = self.long_input[1] * 10 + sound_nr
@@ -46,6 +52,7 @@ class KeyListener(Listener):
             return
         # Numpad .
         if key.vk == 110:
-            self.sound.play(self.long_input[1])
-            self.long_input = [False, 0]
+            if self.long_input[1] > 0:
+                self.sound.play(self.long_input[1])
+                self.long_input = [False, 0]
             return
